91318e8379650902de1eea96a15062812a96e627
Tulasi Sanjeeva <dev.bingeops@posteo.de>
2023-09-28 01:50:21 +0200

    corrected my docker registry

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t blaze14/demo-app:jma-2.0 .'
+        sh 'docker build -t dev1n0ps/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push blaze14/demo-app:jma-2.0'
+        sh 'docker push dev1n0ps/demo-app:jma-2.0'
     }
 }
 
